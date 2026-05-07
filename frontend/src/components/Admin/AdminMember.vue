bf800f5b6e7ba3ebd841068e6d13a4ad634e3167
Jeezzz <pychoi130@gmail.com>
2026-05-07 17:32:01 +0800

    add frontend element for calling api

--- frontend/src/components/Admin/AdminMember.vue
+++ frontend/src/components/Admin/AdminMember.vue
@@ -1,5 +1,66 @@
 <template> 
     <h1>Member</h1>
+    <Button @click="get_member">
+        <span>Get Member</span>
+    </Button>
+    <Button @click="show_add_member_dialog">
+        <span>Add Member</span>
+    </Button>
+
+    <Dialog v-model:visible="add_member_dialog" modal header="Create Member">
+        <span>Enter new member information</span>
+        <div class="input-field">
+            <label for="first_name">First Name</label>
+            <InputText id="first_name" autocomplete="off" />
+        </div>
+        <div class="input-field">
+            <label for="last_name">Last Name</label>
+            <InputText id="last_name" autocomplete="off" />
+        </div>
+        <div class="button-section">
+            <Button type="button" label="Cancel" @click="hide_add_member_dialog"></Button>
+            <Button type="button" label="Save" @click="hide_add_member_dialog(); create_member()"></Button>
+            
+        </div>
+    </Dialog>
+
+
+    <div class="member-table">
+        <DataTable
+            v-model:filters="filters" 
+            :value="customers" paginator :rows="5"
+            :rowsPerPageOptions="[5,10,20,50]"
+            paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+            currentPageReportTemplate="{first} to {last} of {totalRecords}"
+            :globalFilterFields="['name']">
+            <template #header>
+                <InputText v-model="filters['name'].value" placeholder="Name Search" />
+
+            </template>
+
+
+            <template>
+                <Column field="id" header="Member ID"></Column>
+                <Column field="name" header="Name">
+                    <template #body="{ data }">
+                        {{ data.name }}
+                    </template>
+                </Column>
+                <Column field="status" header="Status"></Column>
+                <Column field="created_at" header="Joined Date"></Column>
+                <Column>
+                    <template #body>
+                        <Button>
+
+                        <i class="pi pi-user-edit"></i>
+                    </Button>
+                    </template>
+                </Column>
+            </template>
+        </DataTable>
+    </div>
+
 </template> 
 
 <script src="@/scripts/Admin/AdminMember.js" />
+<style src="@/css/Admin/AdminMember.css" />
--- frontend/src/scripts/Admin/AdminMember.js
+++ frontend/src/scripts/Admin/AdminMember.js
@@ -1,9 +1,97 @@
-import axios from 'axios'
+import axios from 'axios';
+import Button from 'primevue/button';
+
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';   // optional
+import Row from 'primevue/row'; 
+import { FilterMatchMode } from '@primevue/core/api';
+
+import Dialog from 'primevue/dialog';
+import InputText from 'primevue/inputtext';
+
 
 export default {
+  components: {
+    Button,
+    DataTable,
+    Column,
+    ColumnGroup,
+    Row,
+    Dialog,
+    InputText,
+  },
   data() {
-    return {}
+    return {
+      customers: [],
+      add_member_dialog: false,
+      filters: {
+        global: {value: null, matchMode: FilterMatchMode.CONTAINS},
+        name: {value: null, matchMode: FilterMatchMode.STARTS_WITH}
+      }
+    }
+  },
+  mounted() {
+    this.get_member();
   },
   methods: {
+    get_member(){
+      const token = localStorage.getItem('access');
+      try {
+        console.log("Get member item");
+        let res = axios.get('http://127.0.0.1:8000/api/member_item', 
+          {headers: { Authorization: `Bearer ${token}`}}
+        );
+
+        res.then((Response) => {
+
+          console.log(Response.data.data);
+
+          let data = Response.data.data;
+
+          for(let i = 0; i < data.length; i++){
+            data[i].status = data[i].status ? "active" : "inactive"
+            data[i].created_at = data[i].created_at.split('T')[0];
+          }
+          this.customers = data;
+        });
+        
+      }catch{
+        alert("ERROR!")
+      }
+    },
+    create_member(){
+      const token = localStorage.getItem('access');
+      try {
+
+        const first_name_element = document.getElementById('first_name');
+        const last_name_element = document.getElementById('last_name');
+
+        let input_data = {"first_name": first_name_element.value, "last_name": last_name_element.value};
+
+        let res = axios.post('http://127.0.0.1:8000/api/create_member/',
+          input_data,
+          {headers: { Authorization: `Bearer ${token}`}}
+        );
+
+        console.log("Create member done");
+
+
+
+        
+      }catch{
+        alert("ERROR!");
+      }
+
+      // refresh list
+      this.get_member();
+
+    },
+    show_add_member_dialog() {
+      this.add_member_dialog = true;
+    },
+    hide_add_member_dialog() {
+      this.add_member_dialog = false;
+    }
   }
 }
