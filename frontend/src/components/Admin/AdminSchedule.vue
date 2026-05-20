eff5b90008c0ab4c1981633cfc8561a60345945f
Jeezzz <pychoi130@gmail.com>
2026-05-20 15:08:23 +0800

    add calling to schedule

--- frontend/src/components/Admin/AdminSchedule.vue
+++ frontend/src/components/Admin/AdminSchedule.vue
@@ -1,5 +1,67 @@
 <template> 
     <h1>Schedule</h1>
+    <div v-for="day in days" style="width: 25rem">
+        <Card style="width: 25rem">
+            <template #title>{{ day.string }}
+            </template>
+            <template #content>
+                <div v-for="item in day.items" style="width:15rem">
+                    <Card style="width: 15rem">
+                        <template #title>{{ item.name }}</template>
+                        <template #content>Time: {{ item.time }} Attendance: {{ item.max_attendance }}</template>
+                        <template #footer>
+                            <Button>
+                                <i class="pi pi-minus"></i>
+                            </Button>
+                            <Button>
+                                <i class="pi pi-pencil"></i>
+                            </Button>
+                        </template>
+                    </Card>
+                </div>
+            </template>
+            <template #footer>
+                <Button @click="show_create_dialog">
+                    <i class="pi pi-plus"></i>
+                </Button>
+            </template>
+        </Card>
+    </div>
+    <div style="width: 25rem">
+        <Card style="width: 25rem">
+            <template #title>Inactive</template>
+        </Card>
+    </div>
+
+    <Dialog v-model:visible="display_create_dialog" model header="Create Schedule">
+        <div>
+            <Form v-slot="$form" @submut="create_schedule_item">
+                <div>
+                    <label for="schedule_name">Name</label>
+                    <InputText v-model="create_item_form.name" id="schedule_name" autocomplete="off" />
+                </div>
+                <div>
+                    <label>Day</label>
+                    <Select v-model="create_item_form.day" :options="day_options" optionLabel="name" optionValue="name"  fluid></Select>
+                </div>
+                <div>
+                    <label>Time</label>
+                    <DatePicker v-model="create_item_form.time" id="datepicker-timeonly" timeOnly fluid />
+                </div>
+                <div>
+                    <label>Max Attendance</label>
+                    <InputNumber v-model="create_item_form.max_attendance" mode="decimal" showButtons :min="0"></InputNumber>
+                </div>
+                <div class="button-section">
+                    <Button type="button" label="Cancel" @click="hide_create_dialog"></Button>
+                    <Button type="submit" label="Create" @click="hide_create_dialog(); create_schedule_item()"></Button>
+                </div>
+            </Form>
+        </div>
+ 
+    </Dialog>
+
+
 </template> 
 
 <script src="@/scripts/Admin/AdminSchedule.js" />
--- frontend/src/scripts/Admin/AdminSchedule.js
+++ frontend/src/scripts/Admin/AdminSchedule.js
@@ -1,9 +1,52 @@
 import axios from 'axios'
+import Card from 'primevue/card';
+import Button from 'primevue/button';
+import Dialog from 'primevue/dialog';
+import {Form} from '@primevue/forms';
+import Select from 'primevue/select';
+
+import InputText from 'primevue/inputtext';
+import InputNumber from 'primevue/inputnumber';
+import DatePicker from 'primevue/datepicker';
 
 export default {
+  components: {
+    Card,
+    Button,
+    Dialog,
+    Form,
+    InputNumber,
+    InputText,
+    Select,
+    DatePicker
+  },
   data() {
     return {
       schedule: [],
+      days: [{'string': 'Monday', 'id': 0, items: []},
+            {'string': 'Tuesday', 'id': 1, items: []},
+            {'string': 'Wednesday', 'id': 2, items: []},
+            {'string': 'Thrusday', 'id': 3, items: []},
+            {'string': 'Friday', 'id': 4, items: []},
+            {'string': 'Saturday', 'id': 5, items: []},
+            {'string': 'Sunday', 'id': 6, items: []}],
+      day_options: [
+        {name : 'Monday'},
+        {name : 'Tuesday'},
+        {name : 'Wednesday'},
+        {name : 'Thrusday'},
+        {name : 'Friday'},
+        {name : 'Saturday'},
+        {name : 'Sunday'}
+      ],
+      display_create_dialog: false,
+      display_edit_dialog: false,
+      create_item_form: {
+        name: '',
+        day: '',
+        time: '',
+        max_attendance: ''
+      }
     }
   },
   mounted() {
@@ -20,32 +63,54 @@ export default {
 
         res.then((response) => {
           let data = response.data.data;
-          console.log("data:",data);
-          for(let i = 0; i < data.length; i++){
-            this.schedule.push(data[i]);
+          console.log(data);
+          this.days.forEach(day => day.items = data[day.id]);
+
+          // fix time format
+          console.log("fixing time format")
+          for(let i = 0; i < this.days.length;i++){
+            let data_obj = this.days[i];
+            let obj_item_list = data_obj.items
+            for(let j = 0; j < obj_item_list.length; j++){
+              let item = obj_item_list[j];
+              item['time'] = this.string_date_to_time(item['time']);
+            }
           }
-
-          console.log("this.schedule: ", this.schedule);
-
+          console.log("this.days: ", this.days);
         });
       }catch{
         alert("ERROR!");
       };
     },
-    create_schedule_item() {
-      const token = localStorage.getItem('access');
-      try{
-        const res = axios.get('http://127.0.0.1:8000/api/get_all_schedule/',
-          {headers: { Authorization: `Bearer ${token}`}}
-        );
-
-      }catch{
-        alert("ERROR!");
+    create_schedule_item(form_value) {
+      console.log(this.create_item_form);
+      let input_data = {
+        'name' : this.create_item_form.name,
+        'days' : this.create_item_form.day,
+        'time' : this.create_item_form.time.date
       }
+      const token = localStorage.getItem('access');
+
+    },
+    string_date_to_time(string){
+      return string.split("T")[1];
+    },
+    show_create_dialog(){
+      this.display_create_dialog = true;
+    },
+    hide_create_dialog(){
+      this.display_create_dialog = false;
+    },
+    show_edit_dialog(){
+      this.display_edit_dialog = true;
+    },
+    hide_edit_dialog(){
+      this.display_edit_dialog = false;
+    },
+    formatDate(dateObj) {
+      if (!dateObj) return null;
+      return newDate(dateObj).toISOString();
     }
-
-
-
   }
 }
 
